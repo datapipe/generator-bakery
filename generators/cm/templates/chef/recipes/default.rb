cdab8bf048172b94004795c9ee9269af5b413878
john ramos <john.ramos@gmail.com>
2016-11-11 11:48:40 -0800

    updated the sample recipe
    better as a berks example

--- generators/cm/templates/chef/recipes/default.rb
+++ generators/cm/templates/chef/recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright (c) <%= year %> <%= author_name %>, All Rights Reserved.
+
+include_recipe 'apt'
+include_recipe 'nginx'
+
+directory '/var/www/nginx-default' do
+  owner 'www-data'
+  group 'www-data'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+file '/var/www/nginx-default/index.html' do
+  owner 'www-data'
+  group 'www-data'
+  mode '0755'
+  content 'Hello World from Packer!'
+  action :create
+end
