0da7ba9c4dca564d0ebc6fa55bf21b103a22eaf9
Duffy Gillman <dgillman@datapipe.com>
2016-11-15 10:11:33 -0700

    add lib/ directory to test config

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('nsp', function (cb) {
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src('generators/**/*.js')
+  return gulp.src(['generators/**/*.js','lib/*.js'])
     .pipe(excludeGitignore())
     .pipe(istanbul({
       includeUntested: true
@@ -46,7 +46,7 @@ gulp.task('test', ['pre-test'], function (cb) {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
+  gulp.watch(['generators/**/*.js', 'lib/*.js', 'test/**'], ['test']);
 });
 
 gulp.task('prepublish', []);
